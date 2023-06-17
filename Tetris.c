dbb947bb2466774751ba1de8237ba26034b61907
PlppIle <whitney2319@naver.com>
2023-06-18 08:33:39 +0900

    커서 위치 set, get

--- Tetris.c
+++ Tetris.c
@@ -15,6 +15,30 @@
 // 게임보드
 int gameBoardInfo[GBOARD_HEIGHT + 1][GBOARD_WIDTH + 2];
 
+// 현재 커서 위치 저장 변수
+int curPosX, curPosY;
+
+// 현재 마우스 위치 x, y로 변경
+void SetCurrentCursorPos(int x, int y)
+{
+	COORD pos = { x, y };
+
+	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), pos);
+}
+
+// 현재 마우스 위치 받아오기
+COORD GetCurrentCursorPos()
+{
+	CONSOLE_SCREEN_BUFFER_INFO	curInfo;
+	COORD curPoint;
+
+	GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &curInfo);
+	curPoint.X = curInfo.dwCursorPosition.X;
+	curPoint.Y = curInfo.dwCursorPosition.Y;
+
+	return curPoint;
+}
+
 // 시작 시 게임보드 초기화
 void gameBoardinit()
 {
